6a113035d766b261a4729dd23e20d3303b26bff6
sanjai-mahalingan <san.mobiapp.tech@gmail.com>
2024-07-20 21:18:04 +0530

    Registration and Login view added

--- lib/ui/auth/login_view.dart
+++ lib/ui/auth/login_view.dart
@@ -9,11 +9,144 @@ class LoginView extends ConsumerStatefulWidget {
 }
 
 class _LoginView extends ConsumerState<LoginView> {
+  final _formKey = GlobalKey<FormState>();
+  bool isLoading = false;
+  final RegExp emailValid = RegExp(
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  onLogin() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        isLoading = true;
+      });
+    } else {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login View'),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.black87, Colors.black, Colors.black],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome back,',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: const Color.fromARGB(255, 173, 171, 171)),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Image(
+                image: AssetImage('assets/home_needer.jpg'),
+                height: 110,
+                width: 120,
+                opacity: AlwaysStoppedAnimation(.8),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Home Needer',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: const Color.fromARGB(255, 173, 171, 171)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text('Email'),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Email is required";
+                          }
+                          if (value.isNotEmpty && !emailValid.hasMatch(value)) {
+                            return "Enter valid email";
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          label: Text('Password'),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Password is required";
+                          }
+                          if (value.isNotEmpty && value.length <= 6) {
+                            return "Password must contains minimum 6 characters";
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          onLogin();
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white12,
+                            foregroundColor: Colors.blueGrey),
+                        child: const Text('Login'),
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Row(
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, 'registrationView');
+                            },
+                            child: const Text(
+                              "Don't have account? Register here.",
+                              style: TextStyle(color: Colors.blueGrey),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/ui/auth/registration_view.dart
+++ lib/ui/auth/registration_view.dart
@@ -9,11 +9,144 @@ class RegistrationView extends ConsumerStatefulWidget {
 }
 
 class _RegistrationView extends ConsumerState<RegistrationView> {
+  final _formKey = GlobalKey<FormState>();
+  bool isLoading = false;
+  final RegExp emailValid = RegExp(
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  onRegistration() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        isLoading = true;
+      });
+    } else {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Registration View'),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.black87, Colors.black, Colors.black],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Join with,',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: const Color.fromARGB(255, 173, 171, 171)),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Image(
+                image: AssetImage('assets/home_needer.jpg'),
+                height: 110,
+                width: 120,
+                opacity: AlwaysStoppedAnimation(.8),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Home Needer',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: const Color.fromARGB(255, 173, 171, 171)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text('Email'),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Email is required";
+                          }
+                          if (value.isNotEmpty && !emailValid.hasMatch(value)) {
+                            return "Enter valid email";
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          label: Text('Password'),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Password is required";
+                          }
+                          if (value.isNotEmpty && value.length <= 6) {
+                            return "Password must contains minimum 6 characters";
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          onRegistration();
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white12,
+                            foregroundColor: Colors.blueGrey),
+                        child: const Text('Register Now'),
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Row(
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, 'loginView');
+                            },
+                            child: const Text(
+                              'Back to Login',
+                              style: TextStyle(color: Colors.blueGrey),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
